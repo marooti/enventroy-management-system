c28077a5f06c18fff74a166d2d36abb2ce0b834e
Muhammad Imran <88757033+marooti@users.noreply.github.com>
2024-04-29 15:58:25 +0500

    Every things install basic thinks

--- src/app/pages/enventry-management.module.ts
+++ src/app/pages/enventry-management.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
 const routes: Routes = [
   { path: '', component: HomePageComponent },];
 
@@ -14,7 +17,9 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-
+    ButtonModule,
+    MatDatepickerModule,
+    MatButtonModule
   ]
 })
 export class EnventryManagementModule { }
